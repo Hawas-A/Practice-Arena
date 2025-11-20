4b1bddfd3ac6ef3d50e9f8ebcbc2c7446c523a20
Mohamed Rooby <mohamedrooby34@gmail.com>
2025-11-20 14:17:59 +0200

    Added jump and sprint input actions

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,29 +5,64 @@ public class Player : Character
 {
     private CharacterController playerController;
     private InputAction moveAction;
+    private InputAction jumpAction;
+    private InputAction sprintAction;
     private PlayerInput playerInput;
 
+    [Header("Movement Settings")]
+    [SerializeField] private float sprintMultiplier = 2.0f;
+    private float currentSpeed;
 
-    
+    [Header("Jump Settings")]
+    [SerializeField] private float jumpHeight = 2f;
+    [SerializeField] private float gravity = -9.81f;
+
+    private Vector3 velocity; // combines horizontal + vertical
 
     private void Awake()
     {
         playerInput = GetComponent<PlayerInput>();
         moveAction = playerInput.actions["Move"];
-        playerController = GetComponentInChildren<CharacterController>();
+        jumpAction = playerInput.actions["Jump"];
+        sprintAction = playerInput.actions["Sprint"];
 
+        playerController = GetComponentInChildren<CharacterController>();
     }
+
     private void Update()
     {
+      
         Move();
+        Jump();
     }
 
-
-    private void Move()
+   
+    public void Move()
     {
-        Vector2 direction =  moveAction.ReadValue<Vector2>();
-        Vector3 direction3D = new Vector3(direction.x, 0 , direction.y);
-        playerController.Move(direction3D* moveSpeed * Time.deltaTime);
+        Vector2 dir2D = moveAction.ReadValue<Vector2>();
+        Sprint();
+        Vector3 horizontalMove = new Vector3(dir2D.x, 0, dir2D.y) * currentSpeed;
+        Vector3 move = horizontalMove + new Vector3(0, velocity.y, 0);
+        playerController.Move(move * Time.deltaTime);
+    }
+
+    public void Sprint()
+    {
+        bool isSprinting = sprintAction.IsPressed();   
+        currentSpeed = moveSpeed * (isSprinting ? sprintMultiplier : 1f);
+    }
+
+    public void Jump()
+    {
+        if (jumpAction.triggered && playerController.isGrounded)
+        {
+
+            velocity.y = Mathf.Sqrt(-2f * gravity * jumpHeight);
+        }
+
+
+        velocity.y += gravity * Time.deltaTime;
+
     }
 
     public override void Attack()
